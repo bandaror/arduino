800788808c070f435636ea7a63c1b188509061ce
bandaror <37046192+bandaror@users.noreply.github.com>
2019-06-21 18:45:24 +0100

    escape the loop
    buzzer function added
    use the array to make a code made of points and dashes.

--- morse.ino
+++ morse.ino
@@ -1,20 +1,27 @@
-int button = 8;
-int led = 7;
+int button = 6;
+int led = 5;
+int buzzer = 4;
+
+
+
 void setup() {
  pinMode(led, OUTPUT);
  pinMode(button, INPUT_PULLUP);
-
+ pinMode(buzzer, OUTPUT);
 }
+
+
 
 void loop() {
   if(digitalRead(button)== LOW)
   {
     digitalWrite(led, HIGH);
-    
+    digitalWrite(buzzer, HIGH);
   }
   else
   {
     digitalWrite(led, LOW);
-    
+    digitalWrite(buzzer, LOW);
+
   }
 }
---
+++ morse/morse.ino
@@ -0,0 +1,54 @@
+#include <LiquidCrystal.h>
+int button = 6;
+int led = 5;
+int buzzer = 4;
+int counter = 0;
+int cursorPos =0 ;
+LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
+
+
+void setup() {
+ pinMode(led, OUTPUT);
+ pinMode(button, INPUT_PULLUP);
+ pinMode(buzzer, OUTPUT);
+ lcd.begin(16, 2);
+ lcd.print("Morse code beta");
+}
+
+
+
+void loop() {
+    
+    
+  if(digitalRead(button)== LOW)
+  {
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("length:");
+    digitalWrite(led, HIGH);
+    digitalWrite(buzzer, HIGH);
+    lcd.setCursor(7,0);
+    lcd.print(counter/10);
+    counter ++;
+     if (counter < 50)
+    {
+      lcd.setCursor(cursorPos,1);
+      lcd.print(".");
+
+    }
+    if(counter > 50)
+    {
+      lcd.setCursor(cursorPos+1,1);
+      lcd.print("_");
+    }
+  }
+  else
+  {
+    
+    digitalWrite(led, LOW);
+    digitalWrite(buzzer, LOW);
+    
+    counter = 0;
+   
+  }
+}
