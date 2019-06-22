cdc8455e3c2367626312aa2596d854cc9a681b1a
bandaror <37046192+bandaror@users.noreply.github.com>
2019-06-22 14:40:16 +0100

    Update morse.ino

--- morse/morse.ino
+++ morse/morse.ino
@@ -4,7 +4,11 @@ int led = 5;
 int buzzer = 4;
 int counter = 0;
 int cursorPos =0 ;
-int morse = [3];
+char morseArray[10];
+char dash[1] = {'_'};
+char dot[1] = {'.'};
+int cycle = 0; 
+
 LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
 
 
@@ -19,9 +23,9 @@ void setup() {
 
 
 void loop() {
-    
-    
-  if(digitalRead(button)== LOW)
+
+
+  while(digitalRead(button)== LOW)
   {
     lcd.clear();
     lcd.setCursor(0,0);
@@ -31,26 +35,32 @@ void loop() {
     lcd.setCursor(7,0);
     lcd.print(counter/10);
     counter ++;
-     if (counter < 50)
-    {
-      lcd.setCursor(cursorPos,1);
-      lcd.print(".");
+    
 
-    }
-    if(counter > 50)
-    {
-      lcd.setCursor(cursorPos+1,1);
-      lcd.print("_");
-    }
-    
   }
-  else
-  {
-    
+   if (counter < 40)
+    { 
+      morseArray[cycle] = dot[0];
+   
+    }
+    if(counter >= 40)
+    { 
+      morseArray[cycle] = dash[0];
+     
+    }
+   if(counter > 0)
+   {
     digitalWrite(led, LOW);
     digitalWrite(buzzer, LOW);
-    
+    lcd.setCursor(0,1);
+    lcd.print(counter);
+    lcd.setCursor(5,1);
+    lcd.print(morseArray);
+    cycle ++;
+    lcd.setCursor(11,0);
+    lcd.print(cycle);
     counter = 0;
-   
+    
+    
+   }
   }
-}
