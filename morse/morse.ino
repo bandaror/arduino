c9de8e35587870e15ca4eec370300d5ec23cc381
bandaror <37046192+bandaror@users.noreply.github.com>
2020-02-05 20:27:32 +0000

    Update morse.ino

--- morse/morse.ino
+++ morse/morse.ino
@@ -6,21 +6,12 @@ int led = 5;
 int buzzer = 4;
 int counter = 0;
 int cursorPos =0 ;
-char morseArray[6];
-char tempArray[6];
-char dash[1] = {'-'};
-char dot[1] = {'.'};
-int cycle = 0;          //to mark a cycle of a letter (can be len 4 max)
-struct LM{
- char l;
- char m[4];
-};
-//char letters[] =
-//"A  .-,B  -...,C  -.-.,D  -..,E  .,F  ..-.,G  --.,H  ....,I  ..,J  .---,K  -.-,L  .-..,M  --,N  -.,O  ---,P  .--.,Q  --.-,R  .-.,S  ...,T  -,U  ..-,V  ...-,W  .--,X  -..-,Y  -.--,Z  --..";
-static const struct {const char letter, *code;} morseMap[] ={
-{'A', ".-"},
-{'B', "-..."},
-};
+String morseString = "";
+String tempString = "";
+String dash = "-";
+String dot = ".";
+int cycle = 0;          //to mark the cycle of a letter (can be len 4 max)
+
 LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
 
 
@@ -52,64 +43,40 @@ void loop() {
     lcd.print(counter/10);
 
     counter ++;
-
-
   }
+  
+    /* code */
+  
   //if the counter is less then 400 miliseconds (40 centiseconds) the array will add a dot at the actual position of the cycle
-   if (counter < 40) //the short signal
+   if (counter < 40 && counter >0) //the short signal
     {
-      morseArray[cycle] = dot[cycle-1];
-
+     
+      morseString.concat(dot);
+      Serial.println(morseString);
+ 
     }
   //if the counter is more than 40 centisecond the array will add a dash to the actual cycle position
     if(counter >= 40 and counter < 150)  //the long signal
     {
-      morseArray[cycle] = dash[cycle -1];
+      morseString.concat(dash);
+      Serial.println(morseString);
+
 
     }
 
 //this is when we finish with a letter(the cycle) -holding the button longer than 150cs, then copy the array to the temparray
-    if(counter >150)
+    if(counter >150 )
     {
-      for(int i =0; i<=sizeof(morseArray);i++)  // copy the array
-    {
-      (tempArray[i] = morseArray[i]);
-
-    }
+      tempString = morseString; 
       Serial.println("The morse code is done, the current morse is: ");
-      Serial.println(tempArray);
-
- /*    for(int i =0; i<=sizeof(morseArray);i++)  // copy the array
- //   {
-      (morseArray[i] = 'x');
-
+      Serial.println(morseString);
+      morseString = "";
+      Serial.println("The tempstring is: ");
+      Serial.println(tempString);
     }
 
-
-      Serial.println(" ");
-      Serial.println("deleting the morseArray: ");
-      Serial.println(morseArray);
-
-
-     // Serial.println( morseArray);
-      cycle = -1;
-*/
-
-  LM variable = {'A', "......"};
-    for(int i =0; i<=sizeof(morseArray);i++)
-    {
-      if (variable.m[i] == tempArray[i] or tempArray[i] == 'x')
-      {
-        Serial.println("egyezes");
-      }
-      else
-      {
-        Serial.println("nincs egyezes");
-      }
-    }
-
-      }
-   if(counter > 0)
+ 
+    if(counter > 0)
    {
 
 
@@ -118,30 +85,33 @@ void loop() {
     lcd.setCursor(0,1);
     lcd.print(counter);
     lcd.setCursor(5,1);
-    lcd.print(morseArray);
-    Serial.println("The array and cycle: ");
-    Serial.println(cycle+1);
-    Serial.println(morseArray);
-
-    Serial.println(morseArray);
+    lcd.print(morseString);
+    Serial.println("The cycle: " );
+    Serial.println(cycle);
+  
+    
+    
     //Serial.println(letters);
 
     if(cycle<4)
     {
       cycle = cycle+1;
+
     }
     else
     {
       cycle = 0;
-
+ 
     }
+
+   }
     lcd.setCursor(11,0);
     lcd.print(cycle);
 
-
+    
     counter = 0;
-
+ 
 
 
    }
-  }
+   
